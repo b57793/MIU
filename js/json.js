40abe2f67cc42a34d7cd5c8803c19daaa6870a66
Derick Mullis <dmullis5@fullsail.edu>
2013-08-05 18:38:46 -0400

    Initial Commit
    Beginning MIU

---
+++ js/json.js
@@ -0,0 +1,204 @@
+<!-- Derick Mullis Mobile Interfaces and Usability_1308 Project 1 -->
+
+var jsonData = {
+	"item1" : {
+		"archetype" 		: ["Archetype:", "Warrior"],
+		"item" 				: ["Item Name:", "Mega Cleaver"],
+		"type" 				: ["Type:", "weapon"],
+		"classification" 	: ["Classification:", "2 handed"],
+		"quality" 			: ["Quality:", "legendary"],
+		"sellValue" 		: ["Sell Value:", "300"],
+		"dateStashed" 		: ["Date Stashed:", "2013-06-24"],
+		"droppedFrom" 		: ["Dropped From:", "Dark Butcher"]
+	},
+	"item2" : {
+		"archetype" 		: ["Archetype:", "Rogue"],
+		"item" 				: ["Item Name:", "Uber Stabber"],
+		"type" 				: ["Type:", "weapon"],
+		"classification" 	: ["Classification:", "1 handed"],
+		"quality" 			: ["Quality:", "magical"],
+		"sellValue" 		: ["Sell Value:", "350"],
+		"dateStashed" 		: ["Date Stashed:", "2013-06-22"],
+		"droppedFrom" 		: ["Dropped From:", "Sneaky  McDodge 'n Stuff"]
+	},
+	"item3" : {
+		"archetype" 		: ["Archetype:", "Mage"],
+		"item" 				: ["Item Name:", "Robes of WIN"],
+		"type" 				: ["Type:", "armor"],
+		"classification" 	: ["Classification:", "light armor"],
+		"quality" 			: ["Quality:", "legendary"],
+		"sellValue" 		: ["Sell Value:", "300"],
+		"dateStashed" 		: ["Date Stashed:", "2013-06-23"],
+		"droppedFrom" 		: ["Dropped From:", "That one guy who did bad things"]
+	},
+	"item4" : {
+		"archetype" 		: ["Archetype:", "Warrior"],
+		"item" 				: ["Item Name:", "Mega Cleaver2"],
+		"type" 				: ["Type:", "weapon"],
+		"classification" 	: ["Classification:", "2 handed"],
+		"quality" 			: ["Quality:", "legendary"],
+		"sellValue" 		: ["Sell Value:", "300"],
+		"dateStashed" 		: ["Date Stashed:", "2013-06-24"],
+		"droppedFrom" 		: ["Dropped From:", "Dark Butcher"]
+	},
+	"item5" : {
+		"archetype" 		: ["Archetype:", "Rogue"],
+		"item" 				: ["Item Name:", "Uber Stabber2"],
+		"type" 				: ["Type:", "weapon"],
+		"classification" 	: ["Classification:", "1 handed"],
+		"quality" 			: ["Quality:", "magical"],
+		"sellValue" 		: ["Sell Value:", "350"],
+		"dateStashed" 		: ["Date Stashed:", "2013-06-22"],
+		"droppedFrom" 		: ["Dropped From:", "Sneaky  McDodge 'n Stuff"]
+	},
+	"item6" : {
+		"archetype" 		: ["Archetype:", "Mage"],
+		"item" 				: ["Item Name:", "Robes of WIN2"],
+		"type" 				: ["Type:", "armor"],
+		"classification" 	: ["Classification:", "light armor"],
+		"quality" 			: ["Quality:", "legendary"],
+		"sellValue" 		: ["Sell Value:", "300"],
+		"dateStashed" 		: ["Date Stashed:", "2013-06-23"],
+		"droppedFrom" 		: ["Dropped From:", "That one guy who did bad things"]
+	},
+	"item7" : {
+		"archetype" 		: ["Archetype:", "Warrior"],
+		"item" 				: ["Item Name:", "Mega Cleaver3"],
+		"type" 				: ["Type:", "weapon"],
+		"classification" 	: ["Classification:", "2 handed"],
+		"quality" 			: ["Quality:", "legendary"],
+		"sellValue" 		: ["Sell Value:", "300"],
+		"dateStashed" 		: ["Date Stashed:", "2013-06-24"],
+		"droppedFrom" 		: ["Dropped From:", "Dark Butcher"]
+	},
+	"item8" : {
+		"archetype" 		: ["Archetype:", "Rogue"],
+		"item" 				: ["Item Name:", "Uber Stabber3"],
+		"type" 				: ["Type:", "weapon"],
+		"classification" 	: ["Classification:", "1 handed"],
+		"quality" 			: ["Quality:", "magical"],
+		"sellValue" 		: ["Sell Value:", "350"],
+		"dateStashed" 		: ["Date Stashed:", "2013-06-22"],
+		"droppedFrom" 		: ["Dropped From:", "Sneaky  McDodge 'n Stuff"]
+	},
+	"item9" : {
+		"archetype" 		: ["Archetype:", "Mage"],
+		"item" 				: ["Item Name:", "Robes of WIN3"],
+		"type" 				: ["Type:", "armor"],
+		"classification" 	: ["Classification:", "light armor"],
+		"quality" 			: ["Quality:", "legendary"],
+		"sellValue" 		: ["Sell Value:", "300"],
+		"dateStashed" 		: ["Date Stashed:", "2013-06-23"],
+		"droppedFrom" 		: ["Dropped From:", "That one guy who did bad things"]
+	},
+	"item10" : {
+		"archetype" 		: ["Archetype:", "Warrior"],
+		"item" 				: ["Item Name:", "Mega Cleaver4"],
+		"type" 				: ["Type:", "weapon"],
+		"classification" 	: ["Classification:", "2 handed"],
+		"quality" 			: ["Quality:", "legendary"],
+		"sellValue" 		: ["Sell Value:", "300"],
+		"dateStashed" 		: ["Date Stashed:", "2013-06-24"],
+		"droppedFrom" 		: ["Dropped From:", "Dark Butcher"]
+	},
+	"item11" : {
+		"archetype" 		: ["Archetype:", "Rogue"],
+		"item" 				: ["Item Name:", "Uber Stabber4"],
+		"type" 				: ["Type:", "weapon"],
+		"classification" 	: ["Classification:", "1 handed"],
+		"quality" 			: ["Quality:", "magical"],
+		"sellValue" 		: ["Sell Value:", "350"],
+		"dateStashed" 		: ["Date Stashed:", "2013-06-22"],
+		"droppedFrom" 		: ["Dropped From:", "Sneaky  McDodge 'n Stuff"]
+	},
+	"item12" : {
+		"archetype" 		: ["Archetype:", "Mage"],
+		"item" 				: ["Item Name:", "Robes of WIN4"],
+		"type" 				: ["Type:", "armor"],
+		"classification" 	: ["Classification:", "light armor"],
+		"quality" 			: ["Quality:", "legendary"],
+		"sellValue" 		: ["Sell Value:", "300"],
+		"dateStashed" 		: ["Date Stashed:", "2013-06-23"],
+		"droppedFrom" 		: ["Dropped From:", "That one guy who did bad things"]
+	},
+	"item13" : {
+		"archetype" 		: ["Archetype:", "Warrior"],
+		"item" 				: ["Item Name:", "Mega Cleaver5"],
+		"type" 				: ["Type:", "weapon"],
+		"classification" 	: ["Classification:", "2 handed"],
+		"quality" 			: ["Quality:", "legendary"],
+		"sellValue" 		: ["Sell Value:", "300"],
+		"dateStashed" 		: ["Date Stashed:", "2013-06-24"],
+		"droppedFrom" 		: ["Dropped From:", "Dark Butcher"]
+	},
+	"item14" : {
+		"archetype" 		: ["Archetype:", "Rogue"],
+		"item" 				: ["Item Name:", "Uber Stabber5"],
+		"type" 				: ["Type:", "weapon"],
+		"classification" 	: ["Classification:", "1 handed"],
+		"quality" 			: ["Quality:", "magical"],
+		"sellValue" 		: ["Sell Value:", "350"],
+		"dateStashed" 		: ["Date Stashed:", "2013-06-22"],
+		"droppedFrom" 		: ["Dropped From:", "Sneaky  McDodge 'n Stuff"]
+	},
+	"item15" : {
+		"archetype" 		: ["Archetype:", "Mage"],
+		"item" 				: ["Item Name:", "Robes of WIN5"],
+		"type" 				: ["Type:", "armor"],
+		"classification" 	: ["Classification:", "light armor"],
+		"quality" 			: ["Quality:", "legendary"],
+		"sellValue" 		: ["Sell Value:", "300"],
+		"dateStashed" 		: ["Date Stashed:", "2013-06-23"],
+		"droppedFrom" 		: ["Dropped From:", "That one guy who did bad things"]
+	},
+	"item16" : {
+		"archetype" 		: ["Archetype:", "Warrior"],
+		"item" 				: ["Item Name:", "Mega Cleaver6"],
+		"type" 				: ["Type:", "weapon"],
+		"classification" 	: ["Classification:", "2 handed"],
+		"quality" 			: ["Quality:", "legendary"],
+		"sellValue" 		: ["Sell Value:", "300"],
+		"dateStashed" 		: ["Date Stashed:", "2013-06-24"],
+		"droppedFrom" 		: ["Dropped From:", "Dark Butcher"]
+	},
+	"item17" : {
+		"archetype" 		: ["Archetype:", "Rogue"],
+		"item" 				: ["Item Name:", "Uber Stabber6"],
+		"type" 				: ["Type:", "weapon"],
+		"classification" 	: ["Classification:", "1 handed"],
+		"quality" 			: ["Quality:", "magical"],
+		"sellValue" 		: ["Sell Value:", "350"],
+		"dateStashed" 		: ["Date Stashed:", "2013-06-22"],
+		"droppedFrom" 		: ["Dropped From:", "Sneaky  McDodge 'n Stuff"]
+	},
+	"item18" : {
+		"archetype" 		: ["Archetype:", "Mage"],
+		"item" 				: ["Item Name:", "Robes of WIN6"],
+		"type" 				: ["Type:", "armor"],
+		"classification" 	: ["Classification:", "light armor"],
+		"quality" 			: ["Quality:", "legendary"],
+		"sellValue" 		: ["Sell Value:", "300"],
+		"dateStashed" 		: ["Date Stashed:", "2013-06-23"],
+		"droppedFrom" 		: ["Dropped From:", "That one guy who did bad things"]
+	},
+	"item19" : {
+		"archetype" 		: ["Archetype:", "Warrior"],
+		"item" 				: ["Item Name:", "Mega Cleaver7"],
+		"type" 				: ["Type:", "weapon"],
+		"classification" 	: ["Classification:", "2 handed"],
+		"quality" 			: ["Quality:", "legendary"],
+		"sellValue" 		: ["Sell Value:", "300"],
+		"dateStashed" 		: ["Date Stashed:", "2013-06-24"],
+		"droppedFrom" 		: ["Dropped From:", "Dark Butcher"]
+	},
+	"item20" : {
+		"archetype" 		: ["Archetype:", "Rogue"],
+		"item" 				: ["Item Name:", "Uber Stabber7"],
+		"type" 				: ["Type:", "weapon"],
+		"classification" 	: ["Classification:", "1 handed"],
+		"quality" 			: ["Quality:", "magical"],
+		"sellValue" 		: ["Sell Value:", "350"],
+		"dateStashed" 		: ["Date Stashed:", "2013-06-22"],
+		"droppedFrom" 		: ["Dropped From:", "Sneaky  McDodge 'n Stuff"]
+	}
+}
